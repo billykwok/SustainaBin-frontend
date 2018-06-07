a40d281d8a60a0028c9aa3de5b971f116225c470
Billy Kwok <xobkwok@gmail.com>
2018-06-07 11:01:01 +0800

    add keyboard shortcut

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -9,6 +9,17 @@ const connectSocketToStore = store => {
     console.log('throwingAttempt', { thrown, correct });
     store.dispatch(thrown === correct ? incrementExp() : decrementExp());
   });
+
+  document.body.onkeydown = ({ keyCode }: { keyCode: number }) => {
+    switch (keyCode) {
+      case 38:
+        store.dispatch(incrementExp());
+        break;
+      case 40:
+        store.dispatch(decrementExp());
+        break;
+    }
+  };
 };
 
 export default connectSocketToStore;
